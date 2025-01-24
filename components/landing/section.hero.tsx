e9254367cc6a8241ab79466721be34d4073c4ac5
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2025-01-24 15:53:12 +0100

    feat: clean up HeroSection component by removing commented code and enhancing readability; add "use client" directive in Spotlight component

--- components/landing/section.hero.tsx
+++ components/landing/section.hero.tsx
@@ -6,21 +6,9 @@ import { Spotlight } from "../ui/spotlight";
 export function HeroSection() {
   return (
     <section className="py-40 w-full flex md:items-center md:justify-center bg-black/[0.96] bg-grid-white/[0.02] relative overflow-hidden">
-      {/* <SplashCursor /> */}
       <Spotlight />
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="relative z-20 p-4 max-w-7xl mx-auto w-full pt-20 md:pt-0">
-        {/* <GradientText
-            colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-            animationSpeed={7}
-            showBorder={false}
-            className={cn(
-              "text-7xl !font-black -mb-2",
-              dmSerifDisplay.className
-            )}
-          >
-            VitalSync
-          </GradientText> */}
         <Logo
           className="text-center mx-auto"
           classNames="text-4xl md:text-7xl text-primary !font-black lg:!font-bold"
